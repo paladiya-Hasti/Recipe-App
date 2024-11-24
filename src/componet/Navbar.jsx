18de1051d5111d39683a0e9b7f2b897586bcafc2
hasti-Paladiya <hastipaladiya2001@gmail.com>
2024-11-24 23:48:39 +0530

    Recipe-App-01

--- src/componet/Navbar.jsx
+++ src/componet/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import logo from '../assets/img/logo1.png'
+import logo from '../assets/img/logo1 (1).png'
 
 const Navbar = () => {
   return (
